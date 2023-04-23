8ae886ec277aa13e036134ad7fa6cf229703c411
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-23 19:02:19 +0900

    add all files

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,11 @@
-console.log('hello, node.js');
+// ファイルへの書き込み
+// __dirname, __filename
+// path
+
+const fs = require("fs");
+const path = require("path");
+
+console.log(__filename);
+
+fs.writeFileSync(__dirname + "../dist/test.txt", "hello, node.js");
+// console.log("hello, node.js");
