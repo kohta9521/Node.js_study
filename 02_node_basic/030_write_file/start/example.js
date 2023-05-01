749603a20060a5c03f7c7f71be526170ca3f10d4
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-01 23:05:38 +0900

    add dirname and filename reset

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,13 +1,3 @@
 // ファイルへの書き込み
 // __dirname, __filename
-// path
-
-const fs = require("fs");
-const path = require("path");
-
-console.log(__filename);
-
-const distPath = path.resolve(__dirname, "../dist/test.txt");
-
-fs.writeFileSync(__dirname + "../dist/test.txt", "hello, node.js");
-// console.log("hello, node.js");
+console.log("Hello, node.js");
